e00adf4089473cfd9de41b70b7da120e72bc0db2
Donatas Lučiūnas <donatas.luciunas@gmail.com>
2022-06-10 00:42:40 +0300

    More exports

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 export { default as Computed, ComputeFunc } from './computed';
 export { default as Ref } from './ref';
 export { default as Watch } from './watch';
+export { default as Dependency } from './dependency';
+export { default as Dependent } from './dependent';
